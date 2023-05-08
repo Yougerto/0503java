539c463528b4a36c4023fa4ee13281cd57c68825
USER <USER@L39.r1805.tciii.org.tw>
2023-05-08 12:55:54 +0800

    qqq

--- src/tw/org/iii/tutor/JDBC18.java
+++ src/tw/org/iii/tutor/JDBC18.java
@@ -1,50 +1,34 @@
 package tw.org.iii.tutor;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.Properties;
+import java.awt.BorderLayout;
 
-import tw.org.iii.Uclasses.Bike;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
-public class JDBC18 {
-	private static final String USER = "root";
-	private static final String PASSWORD = "root";
-	private static final String URL = "jdbc:mysql://localhost/iii";
-	private static final String SQL_QUERT = 
-		"SELECT * FROM member WHERE id = ?";
+import tw.org.iii.myclasses.MyTable;
+
+public class JDBC18 extends JFrame{
+	private MyTable myTable;
+
+	public JDBC18() {
+
+		setLayout(new BorderLayout());
+
+		try {
+			myTable = new MyTable();
+			JScrollPane jsp = new JScrollPane(myTable);
+			add(jsp, BorderLayout.CENTER);
+
+			setSize(800, 640);
+			setVisible(true);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}catch(Exception e) {
+			System.exit(123);
+		}
+	}
 
 	public static void main(String[] args) {
-		Properties prop = new Properties();
-		prop.put("user", USER); prop.put("password", PASSWORD);
-		try {
-			Connection conn = DriverManager.getConnection(URL, prop);
-			PreparedStatement pstmt = conn.prepareStatement(SQL_QUERT);
-			pstmt.setInt(1, 1);
-			ResultSet rs = pstmt.executeQuery();
-			
-			if (rs.next()) {
-				InputStream in = rs.getBinaryStream("bike");
-				ObjectInputStream oin = new ObjectInputStream(in);
-				Object obj = oin.readObject();
-				if (obj instanceof Bike) {
-					Bike b2 = (Bike)obj;
-					System.out.println(b2);
-				}else {
-					System.out.println("I am not bike");
-				}
-				
-				System.out.println("OK");
-			}else {
-				System.out.println("data ERROR");
-			}
-		}catch(Exception e) {
-			System.out.println(e);
-		}
+		new JDBC18();
 	}
 
 }
